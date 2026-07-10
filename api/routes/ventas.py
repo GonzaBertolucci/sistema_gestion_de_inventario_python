cb23bb2383ed1a9d4e95405c36da292db1082f7c
Gonzalo Bertolucci <gonzalobertolucci.gb@gmail.com>
2026-07-10 19:40:10 -0300

    Finalizado metodos GET POST PUT Y DELETE de ventas.

--- api/routes/ventas.py
+++ api/routes/ventas.py
@@ -1,14 +1,49 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from db.database import get_db
-from schemas.ventas import Crear_venta, Response_venta
-from crud.crud_ventas import crear_nueva_venta
+from schemas.ventas import VentasCreate, Response_venta, VentasUpdate
+from crud.crud_ventas import crear_nueva_venta, obtener_todas_las_ventas, obtener_venta_por_id, actualizar_venta, eliminar_venta
 
 router = APIRouter()
 
 @router.post("/", response_model=Response_venta)
-def registrar_venta(venta: Crear_venta, db: Session = Depends(get_db)):
-  
-    nueva_venta = crear_nueva_venta(db=db, venta_data=venta)
+def registrar_venta(venta: VentasCreate, db: Session = Depends(get_db)):
+    try:
+        nueva_venta = crear_nueva_venta(db=db, venta_data=venta)
+        return nueva_venta
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+        #El str(e) agarra el texto del CRUD
+
+@router.get("/", response_model=List[Response_venta])
+def leer_ventas(db: Session = Depends(get_db)):
+    lista_de_ventas = obtener_todas_las_ventas(db=db)
+    return lista_de_ventas
+
+@router.get("/{ventas_id}", response_model=Response_venta)
+def leer_venta_por_id(venta_id: int, db:Session = Depends(get_db)):
+    venta = obtener_venta_por_id(db=db,venta_id=venta_id)
     
-    return nueva_venta
+    if venta is None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Venta no encontrada")
+    return venta
+    
+@router.put("/{venta_id}", response_model=Response_venta)
+def modificar_venta(venta_id: int, datos_nuevos: VentasUpdate, db: Session = Depends(get_db)):
+    venta_actualizada = actualizar_venta(db=db, venta_id=venta_id, venta_actualizada=datos_nuevos)
+    
+    if venta_actualizada is None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Venta a actualizar no encontrada")
+    
+    return venta_actualizada
+
+@router.delete("/{venta_id}", response_model=Response_venta)
+def borrar_venta(venta_id: int, db: Session = Depends(get_db)):
+    venta_eliminada = eliminar_venta(db=db, venta_id=venta_id)
+    
+    if venta_eliminada is None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "La venta a eliminar no existe")
+    
+    return venta_eliminada    
--- crud/crud_ventas.py
+++ crud/crud_ventas.py
@@ -1,12 +1,24 @@
 from sqlalchemy.orm import Session
 from models.ventas import Ventas
 from models.detalles_de_ventas import Detalles_de_ventas
-from schemas.ventas import Crear_venta
+from schemas.ventas import VentasCreate, VentasUpdate
+from models.producto import Producto
 
-def crear_nueva_venta(db: Session, venta_data: Crear_venta):
+def crear_nueva_venta(db: Session, venta_data: VentasCreate):
+  
+  total_calculado = 0
+  
+  for detalle in venta_data.detalles:
+    producto_db = db.query(Producto).filter(Producto.id_Prod == detalle.id_producto).first()
+    
+    if producto_db is None:
+      raise ValueError(f"El producto con ID {detalle.id_producto} no existe.")
+    
+    total_calculado += detalle.cantidad * producto_db.precio_Venta_Prod
+    
   nueva_venta = Ventas(
     id_usuario = venta_data.id_usuario,
-    total = venta_data.total,
+    total = total_calculado,
     metodo_de_pago = venta_data.metodo_de_pago
   )
   
@@ -15,11 +27,13 @@ def crear_nueva_venta(db: Session, venta_data: Crear_venta):
   db.refresh(nueva_venta)
   
   for detalle in venta_data.detalles:
+    producto_db = db.query(Producto).filter(Producto.id_Prod == detalle.id_producto).first()
+    
     nuevo_detalle = Detalles_de_ventas(
       id_venta = nueva_venta.id_venta,
       id_producto = detalle.id_producto,
       cantidad = detalle.cantidad,
-      precio_x_unidad = detalle.precio_x_unidad
+      precio_x_unidad = producto_db.precio_Venta_Prod
     )
     db.add(nuevo_detalle)
   
@@ -27,3 +41,36 @@ def crear_nueva_venta(db: Session, venta_data: Crear_venta):
   db.refresh(nueva_venta)
   
   return nueva_venta
+
+def obtener_todas_las_ventas(db: Session):
+  return db.query(Ventas).all()
+
+def obtener_venta_por_id(db: Session, venta_id: int):
+  return db.query(Ventas).filter(Ventas.id_venta == venta_id).first()
+
+def eliminar_venta(db: Session, venta_id: int):
+  venta_db = obtener_venta_por_id(db, venta_id)
+  
+  if venta_db is None:
+    return None
+  
+  venta_db.activo = False
+  
+  db.commit()
+  db.refresh(venta_db)
+  
+  return venta_db
+
+def actualizar_venta(db: Session, venta_id: int, venta_actualizada: VentasUpdate):
+  venta_db = obtener_venta_por_id(db, venta_id)
+  
+  if venta_db is None:
+    return None
+  
+  if venta_actualizada.metodo_de_pago is not None:
+    venta_db.metodo_de_pago = venta_actualizada.metodo_de_pago
+    
+  db.commit()
+  db.refresh(venta_db)
+    
+  return venta_db
